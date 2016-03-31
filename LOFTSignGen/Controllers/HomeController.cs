e6cebe6c343010c3fc5997eb4500b2f5fe9a2faf
Arif Khairiansyah <Arif_Khairiansyah@manulife.com>
2016-03-31 04:31:10 -0400

    draft stage

---
+++ LOFTSignGen/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LOFTSignGen.Models;
+using System.IO;
+
+namespace LOFTSignGen.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Generator()
+        {
+            return View();
+        }
+
+        public ActionResult Generate(InfoViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View("Generator");
+            else
+            {
+                byte[] bArray = System.Text.Encoding.UTF8.GetBytes(RenderRazorViewToString("_Template", vm));
+                return File(bArray, "text/html", string.Format("Signature_LOFT_{0}.html", vm.Name.Replace(" ","_")));
+            }
+        }
+
+        public string RenderRazorViewToString(string viewName, object model)
+        {
+            ViewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindPartialView(ControllerContext,
+                                                                         viewName);
+                var viewContext = new ViewContext(ControllerContext, viewResult.View,
+                                             ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+                viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
---
+++ LOFTSignGen/Models/InfoViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LOFTSignGen.Models
+{
+    public class InfoViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Business Title")]
+        public string BusinessTitle { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Business Address")]
+        public string Address { get; set; }
+        [Required]
+        [Display(Name = "Contact Number")]
+        public string Contact { get; set; }
+
+        [Display(Name = "LinkedIn Id")]
+        public string LinkedInId { get; set; }
+    }
+}
